0ec42245e244cedc2d0eb43dd36b13b935312c0c
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-07-16 20:13:52 +0530

    CallStack

---
+++ July_16/CallStackCheck.java
@@ -0,0 +1,27 @@
+package july_16;
+
+
+public class CallStackCheck {
+
+    public static void A(){
+        B();
+        System.out.println("App A me hai");
+    }
+    public static void B(){
+        System.out.println("App B me hai" );
+        C();
+    }
+    public static void C(){
+        System.out.println("App C me hai");
+        D();
+    }
+    public static void D(){
+        System.out.println("App D me hai");
+    }
+
+    public static void main(String[] args) {
+        A();
+    }
+}
+
+
