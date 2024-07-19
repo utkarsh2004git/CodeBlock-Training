4ede1c8f88cf98d2c67334ee27b0b0a81e383cc5
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-07-19 11:16:45 +0530

    Permutation

---
+++ july_18/Permutation.java
@@ -0,0 +1,21 @@
+
+
+public class Permutation {
+    
+    public static void permu(String ques,String ans){
+
+        if(ques.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0;i<ques.length();i++){
+            permu(ques.substring(0,i)+ques.substring(i+1),ans+ques.charAt(i));
+        }
+    }
+    
+    public static void main(String[] args) {
+        String ques="abc";
+        permu(ques, "");
+    }
+}
