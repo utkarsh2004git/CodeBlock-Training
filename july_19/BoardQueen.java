bb24339fe2567ca432febd6247de3965d5dfb968
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-07-19 12:50:39 +0530

    BoardQueen + Combination

--- july_19/BoardQueen.java
+++ july_19/BoardQueen.java
@@ -11,11 +11,30 @@ public class BoardQueen {
             return;
         }
         for (int i = 0; i < arr.length; i++) {
+
             if(arr[i]==false){
-                arr[i]=true;
+                arr[i]=true;  //Queen ko baitha diya
                 BoardPath(arr, qpsf+1, tq,ans+"q"+qpsf+"b"+i+" ");
-                arr[i]=false;
+                arr[i]=false; //Queen ko Utha diya
             }
+            
+        }
+    }
+
+    public static void combinationBoardPath(boolean arr[],int qpsf,int tq,String ans,int last){
+        if(qpsf==tq){
+            System.out.println(ans);
+            return;
+        }
+        for (int i = last; i < arr.length; i++) {
+
+            if(arr[i]==false){
+                arr[i]=true;  //Queen ko baitha diya
+                
+                combinationBoardPath(arr, qpsf+1, tq,ans+"q"+qpsf+"b"+i+" ",i+1);
+                arr[i]=false; //Queen ko Utha diya
+            }
+            
         }
     }
 
@@ -25,6 +44,7 @@ public class BoardQueen {
         int qpsf=0;
         int tq=2;
         String ans="";
-        BoardPath(arr, qpsf, tq, ans);
+        // BoardPath(arr, qpsf, tq, ans);
+        combinationBoardPath(arr, qpsf, tq, ans,0);
     }
 }
